439cc5fe3ecff79e5b0091585d54540bc706cc3d
DanLombardy <lombardy.dan@gmail.com>
2015-11-01 23:24:38 -0800

    Add file upload functionality to server

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ var router = require(__dirname + "/lib/router");
 var requestHandlers = require(__dirname + "/lib/requestHandlers");
 
 var handle = {};
-handle[""] = requestHandlers.start;
+handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
 
 
 server.start(router.route, handle);
